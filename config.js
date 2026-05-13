4bca5d084bd68d0e688aae28bc5d9a8a62203370
oleg684 <oleg@vr-in.net>
2026-05-13 15:44:06 +0200

    Author: update position/scale/rotation

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ const EXHIBITION_CONFIG = {
       title: "Картина 1",
       target: "assets/targets/artwork-1.jpg",
       model: "assets/models/artwork-1.glb",
-      position: { x: 0, y: 0, z: 0 },
-      scale: 0.15,
-      rotation: { x: 0, y: 0, z: 0 }
+      position: { x: -1.03, y: -1.57, z: 0 },
+      scale: 0.14,
+      rotation: { x: 0, y: -77.8, z: 0 }
     }
   ]
 };
