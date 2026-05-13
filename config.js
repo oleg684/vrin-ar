0dfa2429707425a9c32b43223ab11fc06cafa88a
oleg684 <oleg@vr-in.net>
2026-05-13 15:42:23 +0200

    Author: update position/scale/rotation

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 // Конфигурация AR-выставки
-// Все параметры настраиваются здесь без изменения основного кода
-
 const EXHIBITION_CONFIG = {
   artworks: [
     {
@@ -8,11 +6,8 @@ const EXHIBITION_CONFIG = {
       title: "Картина 1",
       target: "assets/targets/artwork-1.jpg",
       model: "assets/models/artwork-1.glb",
-      // Позиция: x=вправо/влево, y=вверх/вниз, z=ближе/дальше
       position: { x: 0, y: 0, z: 0 },
-      // Масштаб: 0.1 = маленький, 1.0 = оригинальный
       scale: 0.15,
-      // Поворот в градусах
       rotation: { x: 0, y: 0, z: 0 }
     }
   ]
