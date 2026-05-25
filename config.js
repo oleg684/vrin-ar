2cad36e7226cf20728458e83dc9279f58b458747
oleg684 <oleg@vr-in.net>
2026-05-25 15:01:32 +0200

    Admin: add scene

--- config.js
+++ config.js
@@ -153,6 +153,23 @@ const EXHIBITION_CONFIG = {
         "y": 0,
         "z": 0
       }
+    },
+    {
+      "id": "scene-mpl7ti6o",
+      "title": "Сцена 10",
+      "target": "",
+      "model": "",
+      "position": {
+        "x": 0,
+        "y": 0,
+        "z": 0
+      },
+      "scale": 0.06,
+      "rotation": {
+        "x": 0,
+        "y": 0,
+        "z": 0
+      }
     }
   ]
 };
