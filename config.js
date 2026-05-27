263295baa67939876828e20fb720e352fa031fd6
oleg684 <oleg@vr-in.net>
2026-05-27 15:31:17 +0200

    Admin: add scene

--- config.js
+++ config.js
@@ -1,86 +1,22 @@
 // Конфигурация AR-выставки
 const EXHIBITION_CONFIG = {
-  artworks: [
+  "artworks": [
     {
-      id: "artwork-4",
-      title: "Картина 1",
-      target: "assets/targets/artwork-4.jpg",
-      model: "assets/models/artwork-4.glb",
-      position: { x: -0.53, y: 0.1, z: 0.06 },
-      scale: 0.08,
-      rotation: { x: 0, y: -86, z: 0 }
-    },
-    {
-      id: "artwork-5",
-      title: "Картина 2",
-      target: "assets/targets/artwork-5.jpg",
-      model: "assets/models/artwork-5.glb",
-      position: { x: -0.05, y: -0.05, z: 0 },
-      scale: 0.2,
-      rotation: { x: 0, y: -72.4, z: 0 }
-    },
-    {
-      id: "artwork-6",
-      title: "Картина 3",
-      target: "assets/targets/artwork-6.jpg",
-      model: "assets/models/artwork-6.glb",
-      position: { x: -0.15, y: -0.4, z: 0 },
-      scale: 0.11,
-      rotation: { x: 0, y: -87.1, z: 0 }
-    },
-    {
-      id: "artwork-7",
-      title: "Картина 4",
-      target: "assets/targets/artwork-7.jpg",
-      model: "assets/models/artwork-7.glb",
-      position: { x: 0.05, y: -0.21, z: 0 },
-      scale: 0.03,
-      rotation: { x: 0, y: -164.9, z: 0 }
-    },
-    {
-      id: "artwork-8",
-      title: "Картина 5",
-      target: "assets/targets/artwork-8.jpg",
-      model: "assets/models/artwork-8.glb",
-      position: { x: 0.07, y: -0.14, z: -0.04 },
-      scale: 0.12,
-      rotation: { x: 0, y: -195.4, z: 0 }
-    },
-    {
-      id: "artwork-9",
-      title: "Картина 6",
-      target: "assets/targets/artwork-9.jpg",
-      model: "assets/models/artwork-9.glb",
-      position: { x: -0.02, y: -0.75, z: -0.02 },
-      scale: 0.25,
-      rotation: { x: 0, y: -199.1, z: 0 }
-    },
-    {
-      id: "artwork-1779705191053",
-      title: "Сцена 7",
-      target: "assets/targets/artwork-1779705191053.jpg",
-      model: "assets/models/artwork-1779705191053.glb",
-      position: { x: 0, y: 0, z: 0 },
-      scale: 0.06,
-      rotation: { x: 0, y: 0, z: 0 }
-    },
-    {
-      id: "artwork-1779705195121",
-      title: "Сцена 8",
-      target: "assets/targets/artwork-1779705195121.jpg",
-      model: "assets/models/artwork-1779705195121.glb",
-      position: { x: 0, y: 0, z: 0 },
-      scale: 0.06,
-      rotation: { x: 0, y: 0, z: 0 }
-    },
-    {
-      id: "artwork-1779708167025",
-      title: "Сцена 9",
-      target: "assets/targets/artwork-1779708167025.jpg",
-      model: "assets/models/artwork-1779708167025.glb",
-      position: { x: 0, y: 0, z: 0 },
-      scale: 0.06,
-      rotation: { x: 0, y: 0, z: 0 }
+      "id": "scene-mpo3rg4w",
+      "title": "Сцена 1",
+      "target": "",
+      "model": "",
+      "position": {
+        "x": 0,
+        "y": 0,
+        "z": 0
+      },
+      "scale": 0.06,
+      "rotation": {
+        "x": 0,
+        "y": 0,
+        "z": 0
+      }
     }
   ]
 };
